855d2258542cc437420f549c895de401a91927e0
Malte <malte@dieheringe.de>
2022-08-02 21:48:39 +0200

    update

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ class WebServer {
                         })
                         files = files.map((file) => {
                             var split1 = file.name.replace(".zip", "").split("#");
-                            return { name: file.name, host: split1[0], date: new Date(split1[1].replace(/,/g, ":").replace(/,/g, ".")) };
+                            return { name: file.name, host: split1[0], date: new Date(split1[1].replace(/_/g, ":").replace(/,/g, ".")) };
                         });
 
                         files.sort((a, b) => {
